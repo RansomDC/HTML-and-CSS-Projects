ec658084af63a8913d13a8c8c1b692f441a397e5
Ransom Cadorette <91761633+RansomDC@users.noreply.github.com>
2022-04-11 13:37:48 -0700

    Added all new files for HTML + JavaScript integration practice

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_2.js
@@ -0,0 +1,6 @@
+function My_First_Function() {
+		 let string = "Kiss me, I'm Irish!";
+		 let result = string.fontcolor("green");
+		 document.getElementById("Irish").innerHTML = 
+		 result;
+		 }
